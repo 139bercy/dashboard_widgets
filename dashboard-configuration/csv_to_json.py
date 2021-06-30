58dbe3e0e2ed724f684dcd4766485389cca7c358
Chabriel <maxime.chabriel@ensae.fr>
2021-06-30 09:40:33 +0200

    Modification Affichage. Correction erreur code tojson config.

--- dashboard-configuration/csv_to_json.py
+++ dashboard-configuration/csv_to_json.py
@@ -5,18 +5,20 @@ import os
 def convert_excel_to_json(path):
     """Exporte un json à partir d'un excel avec comme clef une ligne et comme valeur un dictionnaire: {colonne: value}"""
     df = pd.read_csv(path, sep = ",")
-    liste_finale = []
+    liste_panneaux = []
     panneau_properties = ["Lien_page_mesure", "Nom_mesure_GP", "Volet"]
-    onglet_properties = ["Carte", "Graph", "Nom_indicateur_GP", "Description_mesure"] # + indicateur
+    onglet_properties = ["Carte", "Graph", "Nom_indicateur_GP", "Description_mesure", "Nom_indicateur_GP_long"]
     indicateur_properties = ["Nom_mesure_propilot", "Nom_indicateur_propilot", "Code_mesure", "Unité_indicateur", "Indicateur_principal", "Unité_GP", "Lien_open_source"]
     df = df.sort_values(by=["No_Panneau"], ascending=True)
     panneaux = list(df["No_Panneau"].unique())
     for panneau in panneaux:
-        dict_onglet, dict_panneau = {}, {}
+        dict_panneau = {}
+        liste_onglets = []
         df_panneau = df[df["No_Panneau"] == panneau]
         df_panneau = df_panneau.sort_values(by=["No_Onglet"], ascending=True)
         onglets = list(df_panneau["No_Onglet"].unique())
         for onglet in onglets:
+            dict_onglet = {}
             liste_indicateurs = []
             df_onglet = df_panneau[df_panneau["No_Onglet"] == onglet]
             df = df.sort_values(by=["Indicateur_principal"], ascending=False)
@@ -28,24 +30,25 @@ def convert_excel_to_json(path):
                     dict_indicateur[col] = str(df_indicateur[col])
                 liste_indicateurs += [dict_indicateur]
             for col in onglet_properties:
-                dict_onglet[col] = str(df_panneau[col].iloc[0])
+                dict_onglet[col] = str(df_onglet[col].iloc[0])
                 if col in ["Carte", "Graph"]:
                     if int(df_panneau[col].iloc[0]) == 1:
                         dict_onglet[col] = "true"
                     else:
                         dict_onglet[col] = "false"
             dict_onglet["indicateurs"] = liste_indicateurs
+            liste_onglets += [dict_onglet]
         for col in panneau_properties:
             dict_panneau[col] = str(df_panneau[col].iloc[0])
-        dict_panneau["onglets"] = [dict_onglet]
-        liste_finale += [dict_panneau]
-    return liste_finale
+        dict_panneau["onglets"] = liste_onglets
+        liste_panneaux += [dict_panneau]
+    return liste_panneaux
 
-json_final = convert_excel_to_json(path = os.path.join(os.getcwd(), "dashboard_widgets-all-in-vue", "dashboard-configuration", "Web_edito v_1.csv"))
+json_final = convert_excel_to_json(path = os.path.join(os.getcwd(), "dashboard_widgets", "dashboard-configuration", "Web_edito v_1.csv"))
     
-with open(os.path.join(os.getcwd(), "dashboard_widgets-all-in-vue", "dashboard-configuration", "dashboard-configuration.json"), 'w', encoding="utf8") as f:
+with open(os.path.join(os.getcwd(), "dashboard_widgets", "dashboard-configuration", "dashboard-configuration.json"), 'w', encoding="utf8") as f:
     json.dump(json_final, f, indent=4, ensure_ascii=False, sort_keys=False )
-with open(os.path.join(os.getcwd(), "dashboard_widgets-all-in-vue", "public", "dashboard-configuration.json"), 'w', encoding="utf8") as f:
+with open(os.path.join(os.getcwd(), "dashboard_widgets", "public", "dashboard-configuration.json"), 'w', encoding="utf8") as f:
     json.dump(json_final, f, indent=4, ensure_ascii=False, sort_keys=False )
     
 
