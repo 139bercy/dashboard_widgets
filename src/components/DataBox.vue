14b4cddb35ec94614a3e7a01002fe42cbb765798
wassten <wassim.zouitene@efrei.net>
2021-11-16 15:13:32 +0100

    config pour chaque projet txt databox

--- src/components/DataBox.vue
+++ src/components/DataBox.vue
@@ -11,6 +11,7 @@
         <path v-if="isBlue" d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2zm4 11H8v2h8v-2zm0-4H8v2h8V9z" transform="translate(-1366 -5645) translate(1366 5645)"/>
       </svg>
       <span class="fr-ml-1v"> Soit {{convertFloatToHuman(evolvalue)}} % </span>
+      <!-- A configurer pour chaque projet ^^^ -->
     </div>
   </div>
 
