64a6988df896dc8425d5ce5efc1478d5655b68cc
lsoszynski <lysandre.soszynski@afa.gouv.fr>
2023-09-26 09:05:38 +0000

    Test Arrondis 2

--- src/utils.js
+++ src/utils.js
@@ -12,7 +12,15 @@ export const convertStringToLocaleNumber = function (string) {
 
 export const convertFloatToHuman = function (float) {
   // if (Number.isInteger(parseFloat(float))) {
-    return Math.round(float) // Math.round(float * 100) / 100
+    return Math.round(float * 100) / 100 
+  // } else {
+  //   return parseFloat(parseFloat(float).toFixed(2)).toLocaleString()
+  // }
+}
+
+export const convertFloatToHumanDataBox = function (float) {
+  // if (Number.isInteger(parseFloat(float))) {
+    return Math.round(float) // arrondis Math.round(float * 100) / 100  
   // } else {
   //   return parseFloat(parseFloat(float).toFixed(2)).toLocaleString()
   // }
