ab6fa82bacade5b1c9ae4d6b4ca7889e0f917a2d
kilagrom <eric.simon@afa.gouv.fr>
2024-02-09 14:50:45 +0000

    Création des scripts pour 2 graphiques : Secteurs publics et secteurs privés

--- dashboard-configuration/scripts/AFA/afa.py
+++ dashboard-configuration/scripts/AFA/afa.py
@@ -1,23 +1,35 @@
 import utils.fileManager as fm
+import utils.dataManager as dm
 import pandas as pd
 
-data = fm.import_excel("Test.xlsx",[1])
-# customIndex
-
+data = fm.import_excel("DJ-2014-2020.xlsx",[1])
+dm.prepareData(data)
+    
 # Graphique : Analyse par secteurs publics
-def AnalyseSecteursActivite_Public(normal=False):
-    public = data.iloc[:,data.columns.isin(['secteur économique public concerné ','Fiche '])]
-    public = public.dropna()
-    public = public.rename(columns={"Fiche ": "fiche", "secteur économique public concerné ": "secteurEco"})
-    group = public.groupby('fiche').agg(lambda x: x.unique())
-    final = group.value_counts()
-    print(group)
-    return 0#public.value_counts(normalize=normal)
+def AnalyseSecteursActivite_Public(normal=False): 
+    FichesLieesPublic = data.loc[data.secteurPublic.notna()].groupby('fiche')
+    ActeursParFiche = FichesLieesPublic.secteurPublic.unique() # Attention : On parle ici d'une colonne dont les valeurs sont des listes (c relou)
+    
+    values = data.secteurPublic.dropna().unique()
+    proportion = {key: 0 for key in values}
+    for fiche in ActeursParFiche:
+        for acteurPublic in fiche:
+            proportion[acteurPublic] += 1
+            
+    return pd.Series(proportion,name="Nombre de DJ pour chaque type d'acteur public").sort_values(ascending=False)
 
 # Graphique : Analyse par secteurs privés
 def AnalyseSecteursActivite_Prive(normal=False):
-    prive = data.iloc[:,data.columns=='secteur économique privé concerné ']
-    return prive.value_counts(normalize=normal)
+    FichesLieesPrive = data.loc[data.secteurPrive.notna()].groupby('fiche')
+    ActeursParFiche = FichesLieesPrive.secteurPrive.unique() # Attention : On parle ici d'une colonne dont les valeurs sont des listes (c relou)
+    
+    values = data.secteurPrive.dropna().unique()
+    proportion = {key: 0 for key in values}
+    for fiche in ActeursParFiche:
+        for acteurPrive in fiche:
+            proportion[acteurPrive] += 1
+            
+    return pd.Series(proportion,name="Nombre de DJ pour chaque type d'acteur privé").sort_values(ascending=False)
 
 # Graphique : Répartition public/privé
 def AnalyseSecteursActivite_RepartitionPublicPrive():
